83dab3962f547cadf1d7e65902ed0fe2acbd8dfc
ppk <ppk_student@hotmail.com>
2021-10-07 13:43:13 +1000

    Add project files.

---
+++ Черновик/DB/DB.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Черновик
+{
+    public static class DB
+    {
+        static DBEntities db;
+
+        public static DBEntities Get()
+        {
+            if (db == null)
+                db = new DBEntities();
+            return db;
+        }
+    }
+}
---
+++ Черновик/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Черновик.View;
+
+namespace Черновик
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        static MainWindow instance;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            instance = this;
+            Navigate(new MaterialListView());
+        }
+
+        public static void Navigate(Page page) => 
+            instance.MainFrame.Navigate(page);
+    }
+}
